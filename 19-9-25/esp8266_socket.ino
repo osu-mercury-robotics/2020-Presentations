76a59f28e1e1dff48f87ae739861de693ad53be8
Collin-Thornton <collin.thornton@okstate.edu>
2019-10-02 19:59:05 -0500

    Added last week's

---
+++ 19-9-25/esp8266_socket.ino
@@ -0,0 +1,58 @@
+#include "ESP8266WiFi.h"
+ 
+const char* ssid = "MERCURY";
+const char* password =  "";
+ 
+WiFiServer wifiServer(80);
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+ 
+  delay(1000);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting..");
+  }
+  pinMode(0, OUTPUT);
+  Serial.print("Connected to WiFi. IP:");
+  Serial.println(WiFi.localIP());
+ 
+  wifiServer.begin();
+}
+ 
+void loop() {
+ 
+  WiFiClient client = wifiServer.available();
+  
+  if (client) {
+ 
+    while (client.connected()) {
+ 
+      while (client.available()>0) {
+        char c = client.read();
+        Serial.write(c);
+
+        if(c=='1')
+        {
+          Serial.write(" on");
+          digitalWrite(0, LOW);
+        }
+        if(c=='0')
+        {
+          Serial.write(" off");
+          digitalWrite(0, HIGH);
+        }
+      }
+ 
+      delay(10);
+    }
+ 
+    client.stop();
+    Serial.println("Client disconnected");
+ 
+  }
+}
